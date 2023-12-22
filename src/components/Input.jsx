efe34a1b4acf5d80a0636f101daa441bdd5f1d9e
aryan-edstem <aryan.c@edstem.com>
2023-12-22 06:46:58 +0530

    added time taken

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 import './binarysearch.css';
 
 const Input = () => {
-  const array = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
-  ];
+  const [inputArray, setinputArray] = useState()
+  const [outputArray, setOutputArray] = useState()
   const [elem, setElem] = useState("");
   const [result, setResult] = useState(null);
   const [left, setLeft] = useState();
   const [right,setRight] = useState();
+  const [executionTime, setExecutionTime] = useState(null);
 
+  const handleInputArray= (e) => {
+    console.log(e.target.value);
+    const textFieldValue = e.target.value;
+    setOutputArray(textFieldValue.split(',').map(function(item) {
+    return parseInt(item.trim(), 10);
+    }));
+    }
+
+    
   const binarySearch = (arr, elem) => {
     let low = 0;
     let high = arr.length - 1;
@@ -40,35 +45,47 @@ const Input = () => {
 
   const handleSearch = () => {
     const targetNumber = parseInt(elem, 10);
+    const start = performance.now();
     if (!isNaN(targetNumber)) {
-      const index = binarySearch(array, targetNumber);
+      const index = binarySearch(outputArray, targetNumber);
       setResult(index);
     } else {
       setResult(null);
     }
+    const end = performance.now();
+    const time = end - start;
+    setExecutionTime(time + ' milliseconds');
   };
 
   return (
     <div className="container">
-        <div className="input-container">
-          {array.map((value, index) => (
+        <div className="input-text">
+            <label>Input Sorted Array</label>
+            <textarea
+            type="textarea"
+            value={inputArray}
+            onChange={handleInputArray}
+            />
+        </div>
+        {/* <div className="input-container">
+          {inputArray?.map((value, index) => (
             <div key={index}  className="input-box ">
               {value}
             </div>
           ))}
-        </div>
+        </div> */}
       <div>
         <label>Target Element:</label>
         <input
           type="text"
           value={elem}
           onChange={(e) => setElem(e.target.value)}
-        />
+        /><br/><br/>
         <button onClick={handleSearch}>Search</button>
       </div>
       <div>
         <div className="array-container">
-          {array.map((value, index) => (
+          {outputArray?.map((value, index) => (
             <div key={index}  className={`array-box ${index<left || index>right ? 'inactive' :''}`}>
               {value}
             </div>
@@ -76,7 +93,8 @@ const Input = () => {
           ))}
         </div>
         <label>
-          Result: {result !== null ? `Found at index ${result}` : "Not Found"}
+          Result: {result !== -1 ? `Found at position ${result +1}` : "Not Found"} <br />
+          Elapsed time: {executionTime}
         </label>
       </div>
     </div>
