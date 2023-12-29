3c4fe55ecd64d3b0312115b7ce155210e4a364fb
aryan-edstem <aryan.c@edstem.com>
2023-12-29 09:31:43 +0530

    added modifications

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,53 +1,62 @@
 import React, { useState } from "react";
-import './binarysearch.css';
+import "./binarysearch.css";
 import { useDispatch } from "react-redux";
-import { setHigh, setLow,setArray,setResult,setExecutionTime, incrementNoOfIterations, setNoOfIterations } from "../redux/BinarySlice";
-
+import {
+  setHigh,
+  setLow,
+  setArray,
+  setResult,
+  setExecutionTime,
+  incrementNoOfIterations,
+  setNoOfIterations,
+} from "../redux/BinarySlice";
 
 const Input = () => {
-  const [outputArray, setOutputArray] = useState()
+  const [outputArray, setOutputArray] = useState();
   const [elem, setElem] = useState("");
   const dispatch = useDispatch();
 
-  
-  const handleInputArray= (e) => {
+  const handleInputArray = (e) => {
     const textFieldValue = e.target.value;
-    const testarray = textFieldValue.split(',').filter(item => item.trim() !== '').map((item) => parseInt(item.trim(), 10));
+    const testarray = textFieldValue
+      .split(",")
+      .filter((item) => item.trim() !== "")
+      .map((item) => parseInt(item.trim(), 10));
     setOutputArray(testarray);
     dispatch(setArray(testarray));
-    }
+  };
 
   const binarySearch = (arr, elem) => {
     const start = performance.now();
     dispatch(setArray(outputArray));
-    let low= 0
+    let low = 0;
     dispatch(setLow(low));
-    let high = arr.length-1
+    let high = arr.length - 1;
     dispatch(setHigh(high));
-    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     const searchIteration = async () => {
-    while (low <= high) {
-      dispatch(incrementNoOfIterations(1));
-      let mid = Math.floor((low+high)/2)
-      await delay(500);
-      if (arr[mid] === elem) {
-        dispatch(setLow(mid));
-        dispatch(setHigh(mid));
-        dispatch(setResult(mid));
-        const end = performance.now();
-        const time = end - start;
-        dispatch(setExecutionTime(time));
-        return mid;
-      } else if (arr[mid] < elem) {
-        low = mid + 1;
-        dispatch(setLow(low));
-      } else {
-        high = mid - 1;
-        dispatch(setHigh(high))
+      while (low <= high) {
+        dispatch(incrementNoOfIterations(1));
+        let mid = Math.floor((low + high) / 2);
+        await delay(500);
+        if (arr[mid] === elem) {
+          dispatch(setLow(mid));
+          dispatch(setHigh(mid));
+          dispatch(setResult(mid));
+          const end = performance.now();
+          const time = end - start;
+          dispatch(setExecutionTime(time));
+          return mid;
+        } else if (arr[mid] < elem) {
+          low = mid + 1;
+          dispatch(setLow(low));
+        } else {
+          high = mid - 1;
+          dispatch(setHigh(high));
+        }
       }
-    }
-    dispatch(setResult(-1));
-    return -1;
+      dispatch(setResult(-1));
+      return -1;
     };
     return searchIteration();
   };
@@ -61,20 +70,19 @@ const Input = () => {
 
   return (
     <div className="input-container">
-        <div className="input-text">
-            <label>Input Sorted Array</label>
-            <textarea
-            type="textarea"
-            onChange={handleInputArray}
-            />
-        </div>
+      <div className="input-text">
+        <label>Input Sorted Array</label>
+        <textarea type="textarea" onChange={handleInputArray} />
+      </div>
       <div>
         <label>Target Element:</label>
         <input
           type="text"
           value={elem}
           onChange={(e) => setElem(e.target.value)}
-        /><br/><br/>
+        />
+        <br />
+        <br />
         <button onClick={handleSearch}>Search</button>
       </div>
     </div>
